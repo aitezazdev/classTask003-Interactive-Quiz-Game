064064db326357321a0158903b4c72f00d4080f8
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-09-02 00:16:36 +0500

    5 qs add and ssome css changes

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var data = [
-  { question: "1. what is 2 + 2?", choices: ["3", "4", "5", "6"], answer: 1 },
   {
-    question: "2. my name is?",
-    choices: ["zaz", "khan", "bro", "aitezaz"],
-    answer: 3,
+    question: "5. In programming, what is a 'bug'?",
+    choices: ["A feature", "An error or flaw", "A security measure", "A type of loop"],
+    answer: 1
   },
   {
-    question: "2. best film?",
-    choices: ["The dark knight", "Interstellar", "Inception", "no one"],
-    answer: 0,
+    question: "2. Which planet is known as the Red Planet?",
+    choices: ["Venus", "Mars", "Jupiter", "Saturn"],
+    answer: 1,
+  },
+  {
+    question: "2. Which programming language is known as the 'language of the web'?",
+    choices: ["Java", "Python", "C++", "JavaScript"],
+    answer: 3
+  },
+  {
+    question: "1. What is the capital of France?",
+    choices: ["London", "Berlin", "Paris", "Madrid"],
+    answer: 2
+  },
+  {
+    question: "4. Which company developed the Android operating system?",
+    choices: ["Apple", "Microsoft", "Google", "Samsung"],
+    answer: 2
   },
 ];
 
@@ -42,15 +56,15 @@ function displayQuestion() {
   document.getElementById("choice4").disabled = false;
 
   // reset the cursor to default
-    document.getElementById("choice1").style.cursor = "pointer";
-    document.getElementById("choice2").style.cursor = "pointer";
-    document.getElementById("choice3").style.cursor = "pointer";
-    document.getElementById("choice4").style.cursor = "pointer";
+  document.getElementById("choice1").style.cursor = "pointer";
+  document.getElementById("choice2").style.cursor = "pointer";
+  document.getElementById("choice3").style.cursor = "pointer";
+  document.getElementById("choice4").style.cursor = "pointer";
 
-    if (currentQuestionIndex === data.length - 1) {
-      document.getElementById("next").innerText = "Its Over";
-    }
+  if (currentQuestionIndex === data.length - 1) {
+    document.getElementById("next").innerText = "Its Over";
   }
+}
 
 function checkAnswer(selectedOption) {
   btnClicked = true;
@@ -58,7 +72,7 @@ function checkAnswer(selectedOption) {
     score++;
     alert("correct");
   } else {
-    alert("Try Again!");
+    alert("Wrong!");
   }
   scoreElement.textContent = "Score: " + score;
 
@@ -68,7 +82,7 @@ function checkAnswer(selectedOption) {
     document.getElementById("choice2").disabled = true;
     document.getElementById("choice3").disabled = true;
     document.getElementById("choice4").disabled = true;
-    
+
     // disable btn after 1 click so user cannot select one mcq multipe times
     document.getElementById("choice1").style.cursor = "not-allowed";
     document.getElementById("choice2").style.cursor = "not-allowed";
